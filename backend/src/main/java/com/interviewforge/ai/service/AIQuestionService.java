c5f55ada400ba26e180c71bdf114535900e7c98c
Jawahar Bharathi <cjawaharbharathi@gmail.com>
2026-06-12 23:23:35 +0530

    feat: implement AI question generation module

--- backend/src/main/java/com/interviewforge/ai/service/AIQuestionService.java
+++ backend/src/main/java/com/interviewforge/ai/service/AIQuestionService.java
@@ -2,11 +2,14 @@ package com.interviewforge.ai.service;
 
 import org.springframework.stereotype.Service;
 
+import com.interviewforge.interview.entity.Interview;
 import com.interviewforge.interview.repository.InterviewRepository;
 import com.interviewforge.question.repository.QuestionRepository;
 import java.util.List;
 
 import com.interviewforge.question.entity.Question;
+
+
 
 @Service
 public class AIQuestionService {
@@ -22,9 +25,31 @@ public class AIQuestionService {
         this.interviewRepository = interviewRepository;
     }
 
-    public List<Question> generateQuestions(
-            Long interviewId) {
+    public List<Question> generateQuestions(Long interviewId) {
 
-        return List.of();
-    }
+    Interview interview = interviewRepository.findById(interviewId)
+            .orElseThrow(() ->
+                    new RuntimeException("Interview not found"));
+
+    Question q1 = Question.builder()
+            .questionText("What is Dependency Injection?")
+            .answer("A design pattern where dependencies are provided externally.")
+            .category("Spring Boot")
+            .difficulty("MEDIUM")
+            .interview(interview)
+            .build();
+
+    Question q2 = Question.builder()
+            .questionText("Explain JPA.")
+            .answer("JPA is a Java persistence specification.")
+            .category("Database")
+            .difficulty("EASY")
+            .interview(interview)
+            .build();
+
+    questionRepository.save(q1);
+    questionRepository.save(q2);
+
+    return List.of(q1, q2);
+}
 }
